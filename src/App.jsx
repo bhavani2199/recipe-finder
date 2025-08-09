eda40ba4f516124355c85a91cf876732d8dd2f20
bhavani2199 <bhavaniambrose2199@gmail.com>
2025-08-09 15:24:18 +0800

    Add Redux setup and Axios configuration files

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,15 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 function App() {
-  const [count, setCount] = useState(0);
+  // const router = createBrowserRouter([
+  //   {
+  //     path: "/",
+  //     element: < />,
+  //   },
+  // ]);
 
-  return (
-    <>
-      <h1 className="flex text-red-500 text-6xl">Hello World!</h1>
-    </>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ src/api/apiConstants.js
@@ -0,0 +1,2 @@
+export const APP_ID = "6d66bcc9";
+export const APP_KEY = "af778140c1d2f4873cd71f98c21418dd";
